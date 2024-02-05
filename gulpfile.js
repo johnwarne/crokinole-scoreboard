137332b12c0f2388676b6a08f3195a47f0cba207
John Warne <johnmwarne@gmail.com>
2024-02-05 10:09:17 -0700

    Concatenate js files, purge unused CSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,35 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
+const gulp        = require('gulp');
+const sass        = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss');
 const browserSync = require('browser-sync').create();
+const concat      = require('gulp-concat');
+const uglify      = require('gulp-uglify');
 
 // Sass task to compile style.original.scss to style.css
 gulp.task('sass', function() {
   return gulp.src('scoreboard/css/style.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(purgecss({
+      content: ['scoreboard/index.html'],
+      safelist: {
+        deep: [/modal-message$/],
+      },
+      variables: true
+    }))
     .pipe(gulp.dest('scoreboard/css'))
     .pipe(browserSync.stream());
+});
+
+// Concat task to concatenate the JS files
+gulp.task('concat', function() {
+  return gulp.src([
+      'scoreboard/js/vue.min.js',
+      'scoreboard/js/confetti.browser.min.js',
+      'scoreboard/js/script.js'
+    ])
+    .pipe(concat('scoreboard/js/script.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./'));
 });
 
 // Watch task to automatically compile Sass and reload on changes
@@ -17,12 +39,17 @@ gulp.task('watch', function() {
     port: 3000,
     open: false,
   });
-  gulp.watch('**/*.scss', gulp.series('sass'));
-  gulp.watch('**/*.css').on('change', browserSync.reload);
-  gulp.watch('*.php').on('change', browserSync.reload);
-  gulp.watch('*.html').on('change', browserSync.reload);
-  gulp.watch('**/*.js').on('change', browserSync.reload);
+  gulp.watch(['**/*.scss'], gulp.series('sass'));
+  gulp.watch([
+    '**/*.js',
+    '!scoreboard/js/script.min.js'
+  ], gulp.series('concat'));
+  gulp.watch([
+    'scoreboard/css/style.css',
+    'scoreboard/js/script.min.js',
+    'scoreboard/index.html'
+  ]).on('change', browserSync.reload);
 });
 
-// Default task to run the watch task
-gulp.task('default', gulp.series('sass', 'watch'));
+// Default task to run the sass, concat, and watch tasks
+gulp.task('default', gulp.series('sass', 'concat', 'watch'));
